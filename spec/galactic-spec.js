478310248c0dd14852b1f0c514c916d153463ebf
bv <bellavanillo@gmail.com>
2018-04-20 14:37:33 -0700

    add html

---
+++ spec/galactic-spec.js
@@ -0,0 +1,10 @@
+import { Birthdate } from './../src/galactic.js';
+
+
+describe('Birthdate', function(){
+  it('should calculate the age of someone into seconds', function(){
+    let birthdate = new Birthdate();
+    expect(birthdate.secondsAge(15)).toEqual(473040000)
+  });
+  
+});
