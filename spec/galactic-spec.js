8c8c1b9fbc3b562c9cce1d402e6db5da1a0c5cc1
bv <bellavanillo@gmail.com>
2018-04-20 16:01:15 -0700

    write spec for calculation between two days

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -4,7 +4,30 @@ import { Birthdate } from './../src/galactic.js';
 describe('Birthdate', function(){
   it('should calculate the age of someone into seconds', function(){
     let birthdate = new Birthdate();
-    expect(birthdate.secondsAge(15)).toEqual(473040000)
+    expect(birthdate.secondsAge(15)).toEqual(473040000);
   });
-  
+  it('should calculate the age of someone on Mercury', function(){
+    let birthdate= new Birthdate();
+    expect(birthdate.ageMecury(15)).toEqual(62);
+  });
+  it('should calculate the age of someone on Venus', function(){
+    let birthdate= new Birthdate();
+    expect(birthdate.ageVenus(15)).toEqual(24);
+  });
+  it('should calculate the age of someone on Mars', function(){
+    let birthdate= new Birthdate();
+    expect(birthdate.ageMars(15)).toEqual(7);
+  });
+  it('should calculate the age of someone on Jupiter', function(){
+    let birthdate= new Birthdate();
+    expect(birthdate.ageJupiter(15)).toEqual(1);
+  });
+  // it('should calculate the life expectancy of someone based on their gender', function(){
+  //   let birthdate= new Birthdate();
+  //   expect(birthdate.lifeExpectancy(15)).toEqual(78);
+  // });
+  it('should calculate the seconds between two days', function(){
+    let birthdate= new Birthdate();
+    expect(birthdate.differenDate("April 20, 2018 03:45:00", "March 13, 1997 03:45:00")).toEqual(665967600000);
+  });
 });
--- src/galactic.js
+++ src/galactic.js
@@ -1,6 +1,8 @@
 export class Birthdate {
-  constructor () {
+  constructor(age) {
+    this.age = age;
 }
+
   secondsAge(age){
     let yearSeconds = (age * 31536000);
     return yearSeconds;
@@ -18,7 +20,22 @@ export class Birthdate {
     return marsAge;
   }
   ageJupiter(age){
-    let jupiterAge = (Math.floor(age/1.88));
+    let jupiterAge = (Math.floor(age/11.86));
     return jupiterAge;
   }
+  differenDate(current, random){
+    let currentTime = new Date(current).getTime();
+    let randomDate = new Date(random).getTime();
+    let diffDate = (currentTime - randomDate);
+    return diffDate;
+  }
+  // lifeExpectancy(age){
+  //   let yearsLeftMan = (age/78 * 78);
+  //   let yearsLeftWoman = (age/81 *81);
+  //   if ("#gender" === "man") {
+  //     return yearsLeftMan;
+  //   } else if ("#gender" === "woman") {
+  //     return yearsLeftWoman;
+  //   }
+  // }
 }
